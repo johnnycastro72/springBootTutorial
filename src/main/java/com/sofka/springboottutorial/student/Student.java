34749accbe648f842dd1368ea7ee71b5d2ce2b69
Jhonny Castro Clavijo <johnny.castro@misena.edu.co>
2022-05-04 23:04:11 -0500

    Spring Boot Tutorial
    All CRUD methods implented

--- src/main/java/com/sofka/springboottutorial/student/Student.java
+++ src/main/java/com/sofka/springboottutorial/student/Student.java
@@ -2,6 +2,7 @@ package com.sofka.springboottutorial.student;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.time.Period;
 
 @Entity
 @Table
@@ -26,19 +27,17 @@ public class Student {
     public Student() {
     }
 
-    public Student(Long id, String name, String email, LocalDate dob, Integer age) {
+    public Student(Long id, String name, String email, LocalDate dob) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
     }
 
-    public Student(String name, String email, LocalDate dob, Integer age) {
+    public Student(String name, String email, LocalDate dob) {
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
     }
 
     public Long getId() {
@@ -74,11 +73,8 @@ public class Student {
     }
 
     public Integer getAge() {
-        return age;
-    }
 
-    public void setAge(Integer age) {
-        this.age = age;
+        return Period.between(this.dob, LocalDate.now()).getYears();
     }
 
     @Override
